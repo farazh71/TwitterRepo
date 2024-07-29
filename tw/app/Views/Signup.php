7adf1f20455433c311d720dbcc1f2e1e3da3c553
Umair <umairkhan198@gmail.com>
2024-07-29 22:01:27 +0530

    code optimised

--- tw/app/Views/Signup.php
+++ tw/app/Views/Signup.php
@@ -9,8 +9,9 @@
 <body>
     <div id="signup">
         <div class="header">
+            <button id="backBtn" class="info-btn hidden" onclick="backToStartForm()">Back</button>
             <i class="fa-brands fa-twitter my-icon"></i>
-            <button class="sup-btn" id="next" type="button">Next</button>
+            <button class="action-btn" id="nextBtn" type="button">Next</button>
         </div>
         <div class="title">Create your account</div>
 
@@ -24,11 +25,11 @@
         </div>
 
         <div class="end-form hidden" id="endForm">
-            <input type="text" id="userName" placeholder="Name">
-            <input type="password" id="password" placeholder="Password">
-            <input type="date" id="dob" placeholder="Date of Birth">
-            <button id="backBtn" onclick="backToStartForm()">Back</button>
-            <button type="submit" id="submit" onclick="submitData()">Submit</button>
+            <input type="text" class="underline-input" id="userName" placeholder="User Name">
+            <input type="password" class="underline-input"  id="password" placeholder="Password">
+            <input type="date" class="underline-input" id="dob" placeholder="Date of Birth">
+            <textarea class="underline-input" id="bio" placeholder="Enter Bio"></textarea>
+            <button type="submit" class="action-btn" id="submit" onclick="submitData()">Submit</button>
         </div>
     </div>
 
@@ -36,10 +37,14 @@
         let userObj = {};
         let endForm = document.getElementById("endForm");
         let startForm = document.getElementById("startForm");
+        let nextBtn = document.getElementById("nextBtn");
+        let backBtn = document.getElementById("backBtn");
 
         function backToStartForm() {
             startForm.classList.remove("hidden");
             endForm.classList.add("hidden");
+            nextBtn.classList.remove("hidden");
+            backBtn.classList.add("hidden");
         }
 
         document.getElementById('switch').addEventListener('click', function() {
@@ -60,13 +65,13 @@
             var name = document.getElementById('name').value;
             var phone = document.getElementById('phone').value;
             if (name && phone) {
-                document.getElementById('next').removeAttribute('disabled');
+                document.getElementById('nextBtn').removeAttribute('disabled');
             } else {
-                document.getElementById('next').setAttribute('disabled', 'true');
+                document.getElementById('nextBtn').setAttribute('disabled', 'true');
             }
         }
 
-        document.getElementById('next').addEventListener('click', function() {
+        document.getElementById('nextBtn').addEventListener('click', function() {
             var name = document.getElementById('name').value;
             var phoneOrEmail = document.getElementById('phone').value;
             userObj.name = name;
@@ -74,6 +79,8 @@
 
             startForm.classList.add("hidden");
             endForm.classList.remove("hidden");
+            nextBtn.classList.add("hidden");
+            backBtn.classList.remove("hidden");
         });
 
         document.getElementById('name').addEventListener('input', function() {
